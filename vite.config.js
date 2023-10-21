24c7a3ed9cb5a1304b0ad7c0844c546e4f1c69b0
Cristian Aguirre <107177715+caguirre5@users.noreply.github.com>
2023-10-21 15:09:10 -0600

    Done

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/nombre-de-repositorio/', // Reemplaza 'nombre-de-repositorio' con el nombre de tu repositorio
   plugins: [react()],
-})
+});
