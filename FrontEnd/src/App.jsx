50561582452e064f1bd89f4f5c6c2218d95a14a1
Cristian Aguirre <107177715+caguirre5@users.noreply.github.com>
2023-10-21 13:56:04 -0600

    Done

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -8,17 +8,17 @@ function App() {
     area: 100,
     rooms: 3,
     bathroom: 2,
-    parkingSpaces: 1,
+    parking_spaces: 1,
     floor: 4,
-    animalAcept: 0,
-    animalNotAcept: 1,
-    furnitureFurnished: 0,
-    furnitureNotFurnished: 1,
-    cityBeloHorizonte: 1,
-    cityCampinas: 0,
-    cityPortoAlegre: 0,
-    cityRiodeJaneiro: 0,
-    citySãoPaulo: 0,
+    animal_acept: 0,
+    animal_not_acept: 1,
+    furniture_furnished: 0,
+    furniture_not_furnished: 1,
+    city_Belo_Horizonte: 1,
+    city_Campinas: 0,
+    city_Porto_Alegre: 0,
+    city_Rio_de_Janeiro: 0,
+    city_Sao_Paulo: 0,
     hoa: 0, // Valor inicial
     rentAmount: 0, // Valor inicial
     propertyTax: 0, // Valor inicial
@@ -66,20 +66,20 @@ function App() {
     let inputValue = type === 'checkbox' ? (checked ? 1 : 0) : value;
     // Verificar si el checkbox es "animalAcept" o "furnitureFurnished" y actualizar el valor opuesto.
     if (name === 'animalAcept') {
-      setFormData({ ...formData, [name]: inputValue, animalNotAcept: inputValue === 1 ? 0 : 1 });
+      setFormData({ ...formData, [name]: inputValue, animal_not_acept: inputValue === 1 ? 0 : 1 });
     } else if (name === 'furnitureFurnished') {
-      setFormData({ ...formData, [name]: inputValue, furnitureNotFurnished: inputValue === 1 ? 0 : 1 });
+      setFormData({ ...formData, [name]: inputValue, furniture_not_furnished: inputValue === 1 ? 0 : 1 });
     } else if (name == 'selectedCity') {
       if (value == 'Belo Horizonte') {
-        setFormData({ ...formData, cityBeloHorizonte: 1, cityCampinas:  0, cityPortoAlegre : 0, cityRiodeJaneiro : 0, citySãoPaulo : 0 });
+        setFormData({ ...formData, city_Belo_Horizonte: 1, city_Campinas:  0, city_Porto_Alegre : 0, city_Rio_de_Janeiro : 0, city_Sao_Paulo : 0 });
       } else if (value == 'Campinas') {
-        setFormData({ ...formData, cityBeloHorizonte: 0, cityCampinas:  1, cityPortoAlegre : 0, cityRiodeJaneiro : 0, citySãoPaulo : 0 });
+        setFormData({ ...formData, city_Belo_Horizonte: 0, city_Campinas:  1, city_Porto_Alegre : 0, city_Rio_de_Janeiro : 0, city_Sao_Paulo : 0 });
       } else if (value == 'Porto Alegre') {
-        setFormData({ ...formData, cityBeloHorizonte: 0, cityCampinas:  0, cityPortoAlegre : 1, cityRiodeJaneiro : 0, citySãoPaulo : 0 });
+        setFormData({ ...formData, city_Belo_Horizonte: 0, city_Campinas:  0, city_Porto_Alegre : 1, city_Rio_de_Janeiro : 0, city_Sao_Paulo : 0 });
       } else if (value == 'Rio de Janeiro') {
-        setFormData({ ...formData, cityBeloHorizonte: 0, cityCampinas:  0, cityPortoAlegre : 0, cityRiodeJaneiro : 1, citySãoPaulo : 0 });
+        setFormData({ ...formData, city_Belo_Horizonte: 0, city_Campinas:  0, city_Porto_Alegre : 0, city_Rio_de_Janeiro : 1, city_Sao_Paulo : 0 });
       } else {
-        setFormData({ ...formData, cityBeloHorizonte: 0, cityCampinas:  0, cityPortoAlegre : 0, cityRiodeJaneiro : 0, citySãoPaulo : 1 });
+        setFormData({ ...formData, cityBeloHorizonte: 0, city_Campinas:  0, city_Porto_Alegre : 0, city_Rio_de_Janeiro : 0, city_Sao_Paulo : 1 });
       }
 
     } else {
@@ -88,126 +88,135 @@ function App() {
   };
 
   return (
-    <>
-      <aside>
-        <form onSubmit={handleSubmit}>
+    <div>
+      <h2>Cotizador de Rentas</h2>
+      <div className='container'>
+        <aside>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>
+                Área:
+                <input
+                  type="number"
+                  name="area"
+                  value={formData.area}
+                  onChange={handleInputChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Habitaciones:
+                <input
+                  type="number"
+                  name="rooms"
+                  value={formData.rooms}
+                  onChange={handleInputChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Baños:
+                <input
+                  type="number"
+                  name="bathroom"
+                  value={formData.bathroom}
+                  onChange={handleInputChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Plazas de Aparcamiento:
+                <input
+                  type="number"
+                  name="parking_spaces"
+                  value={formData.parking_spaces}
+                  onChange={handleInputChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Piso:
+                <input
+                  type="number"
+                  name="floor"
+                  value={formData.floor}
+                  onChange={handleInputChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label className='checkbox_input'>
+                Animal permitido:
+                <input
+                  type="checkbox"
+                  name="animalAcept"
+                  onChange={handleInputChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label className='checkbox_input'>
+                Amueblado:
+                <input
+                  type="checkbox"
+                  name="furnitureFurnished"
+                  onChange={handleInputChange}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                Ciudad:
+                <select
+                  name="selectedCity"
+                  value={formData.city_Belo_Horizonte}
+                  onChange={handleInputChange}
+                >
+                  <option value="Belo Horizonte">Belo Horizonte</option>
+                  <option value="Campinas">Campinas</option>
+                  <option value="Porto Alegre">Porto Alegre</option>
+                  <option value="Rio de Janeiro">Rio de Janeiro</option>
+                  <option value="São Paulo">São Paulo</option>
+                </select>
+              </label>
+            </div>
+            <button type="submit">Enviar</button>
+          </form>
+        </aside>
+        <div className="result">
           <div>
-            <label>
-              Área:
-              <input
-                type="number"
-                name="area"
-                value={formData.area}
-                onChange={handleInputChange}
-              />
-            </label>
+            <label>Impuesto HOA (R$):</label>
+            <span>{formData.hoa}</span>
           </div>
           <div>
-            <label>
-              Habitaciones:
-              <input
-                type="number"
-                name="rooms"
-                value={formData.rooms}
-                onChange={handleInputChange}
-              />
-            </label>
+            <label>Renta parcial (R$):</label>
+            <span>{formData.rentAmount}</span>
           </div>
           <div>
-            <label>
-              Baños:
-              <input
-                type="number"
-                name="bathroom"
-                value={formData.bathroom}
-                onChange={handleInputChange}
-              />
-            </label>
+            <label>Impuesto sobre bienes inmuebles (R$):</label>
+            <span>{formData.propertyTax}</span>
           </div>
           <div>
-            <label>
-              Plazas de Aparcamiento:
-              <input
-                type="number"
-                name="parkingSpaces"
-                value={formData.parkingSpaces}
-                onChange={handleInputChange}
-              />
-            </label>
+            <label>Seguro contra incendios (R$):</label>
+            <span>{formData.fireInsurance}</span>
           </div>
           <div>
-            <label>
-              Piso:
-              <input
-                type="number"
-                name="floor"
-                value={formData.floor}
-                onChange={handleInputChange}
-              />
-            </label>
+            <label>Renta total (R$):</label>
+            <span>{formData.total}</span>
           </div>
-          <div>
-            <label>
-              Animal permitido:
-              <input
-                type="checkbox"
-                name="animalAcept"
-                onChange={handleInputChange}
-              />
-            </label>
+          <h3>Visualización de los datos</h3>
+          <div className='graphs'>
+            <img src="/src/assets/tendencias_alquiler.png" alt="Mi Imagen1" />
+            <img src="/src/assets/correlacion_total.png" alt="Mi Imagen2" />
           </div>
-          <div>
-            <label>
-              Amueblado:
-              <input
-                type="checkbox"
-                name="furnitureFurnished"
-                onChange={handleInputChange}
-              />
-            </label>
-          </div>
-          <div>
-            <label>
-              Ciudad:
-              <select
-                name="selectedCity"
-                value={formData.selectedCity}
-                onChange={handleInputChange}
-              >
-                <option value="Belo Horizonte">Belo Horizonte</option>
-                <option value="Campinas">Campinas</option>
-                <option value="Porto Alegre">Porto Alegre</option>
-                <option value="Rio de Janeiro">Rio de Janeiro</option>
-                <option value="São Paulo">São Paulo</option>
-              </select>
-            </label>
-          </div>
-          <button type="submit">Enviar</button>
-        </form>
-      </aside>
-      <div className="result">
-        <div>
-          <label>hoa (R$):</label>
-          <span>{formData.hoa}</span>
-        </div>
-        <div>
-          <label>rent amount (R$):</label>
-          <span>{formData.rentAmount}</span>
-        </div>
-        <div>
-          <label>property tax (R$):</label>
-          <span>{formData.propertyTax}</span>
-        </div>
-        <div>
-          <label>fire insurance (R$):</label>
-          <span>{formData.fireInsurance}</span>
-        </div>
-        <div>
-          <label>total (R$):</label>
-          <span>{formData.total}</span>
         </div>
       </div>
-    </>
+    </div>
+    
   )
 }
 
